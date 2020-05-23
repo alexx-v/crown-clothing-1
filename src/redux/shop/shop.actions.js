df7df160381d588334c3f9c6f6165ed17a155867
alexx-v <aleksandr1vlk@gmail.com>
2020-05-23 16:26:26 +0300

    added react-thunk

--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -1,6 +1,36 @@
 import ShopActionTypes from './shop.types';
 
-export const updateCollections = (collectionsMap) => ({
-	type: ShopActionTypes.UPDATE_COLLECTIONS,
+import {
+	firestore,
+	convertCollectionsSnapshotToMap,
+} from '../../firebase/firebase.utils';
+
+export const fetchCollectionsStart = () => ({
+	type: ShopActionTypes.FETCH_COLLECTIONS_START,
+});
+
+export const fetchCollectionsSuccess = (collectionsMap) => ({
+	type: ShopActionTypes.FETCH_COLLECTIONS_SUCCESS,
 	payload: collectionsMap,
 });
+
+export const fetchCollectionsFailure = (errormessage) => ({
+	type: ShopActionTypes.fetchCollectionsFailure,
+	payload: errormessage,
+});
+
+export const fetchCollectionsStartAsync = () => {
+	return (dispatch) => {
+		const collectionRef = firestore.collection('collections');
+		// Изменяет значение isFetching на true. Запускается спиннер.
+		dispatch(fetchCollectionsStart());
+
+		collectionRef
+			.get()
+			.then((snapshot) => {
+				const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+				dispatch(fetchCollectionsSuccess(collectionsMap));
+			})
+			.catch((error) => dispatch(fetchCollectionsFailure(error.message)));
+	};
+};
